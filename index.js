ea2a70f879210ee4558d169c96ea756d7abe0c77
Dioni Mercado <me@dionimercado.com>
2018-01-21 19:51:20 +0000

    Concurrently setup

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('It works!');
 });
 
-app.listen( process.env.PORT || 7777, () => {
-    console.log('Server up and running on port', process.env.PORT || 7777)
+app.listen(8081, () => {
+    console.log('Server running...');
 });
